1abc90a3efc13240dd5fe03a65a95f56f103e6b4
Abudlrahman Almajdalawi <at.majdalawi@gmail.com>
2026-02-25 05:44:13 +0100

    added new test case for wrong sntences, to compare overall scores

--- tests/test_custom.py
+++ tests/test_custom.py
@@ -22,9 +22,12 @@ from sentence_transformers import SentenceTransformer
 
 nl_sents = Path(__file__).parent / "nl_texts.txt"
 en_sents = Path(__file__).parent / "en_texts.txt"
+en_wrong = Path(__file__).parent / "en_wrong.txt"
 
 en = en_sents.read_text(encoding="utf-8").splitlines()
 nl = nl_sents.read_text(encoding="utf-8").splitlines()
+en_wrong = en_wrong.read_text(encoding="utf-8").splitlines()
+
 
 def split_sentences(text):
     """Sentence splitter on commas, points and question marks."""
@@ -34,6 +37,8 @@ def split_sentences(text):
 
 en = [sent for line in en for sent in split_sentences(line)]
 nl = [sent for line in nl for sent in split_sentences(line)]
+en_wrong = [sent for line in en_wrong for sent in split_sentences(line)]
+
 
 if __name__ == "__main__":
 
@@ -41,13 +46,23 @@ if __name__ == "__main__":
     print("Chosen device:", device)
     model = SentenceTransformer(
         "sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2",
+        # "intfloat/multilingual-e5-base",
         device=device,
     )
     print(f"Model device: {next(model._first_module().auto_model.parameters()).device}")
     result = sentalign(en, nl, encoder=model, alignment_max_size=8)
-    print(result)
-    print("Alignments:")
-    for alignment in result.alignments:
-        print(f"  src_indices={alignment.src_indices}, tgt_indices={alignment.tgt_indices}, score={alignment.score:.4f}")
-        print("    src:", " | ".join(en[i] for i in alignment.src_indices))
-        print("    tgt:", " | ".join(nl[i] for i in alignment.tgt_indices))
+    # print(result)
+    # print("Alignments:")
+    # for alignment in result.alignments:
+    #     print(f"  src_indices={alignment.src_indices}, tgt_indices={alignment.tgt_indices}, score={alignment.score:.4f}")
+    #     print("    src:", " | ".join(en[i] for i in alignment.src_indices))
+    #     print("    tgt:", " | ".join(nl[i] for i in alignment.tgt_indices))
+    #     # print final score
+    print(f"Overall score: {result.overall_score:.4f}")
+    print(f"Average alignment score: {result.average_alignment_score:.4f}")
+
+    # now to compare nl with en_wrong, printing only the average alignment score for brevity and overall score
+    result_wrong = sentalign(en_wrong, nl, encoder=model, alignment_max_size=8)
+    print(f"\nComparing nl with en_wrong:")
+    print(f"Overall score: {result_wrong.overall_score:.4f}")   
+    print(f"Average alignment score: {result_wrong.average_alignment_score:.4f}")
